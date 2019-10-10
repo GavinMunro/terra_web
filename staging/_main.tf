8a2551dcb7b7fc6a96f46026b61f8f424eebe7c6
gavin <ob1.data@gmail.com>
2019-10-10 13:51:25 +1000

    Comment on location for AWS access key

--- staging/_main.tf
+++ staging/_main.tf
@@ -6,3 +6,6 @@ resource "aws_key_pair" "key" {
   key_name   = "${var.key_name}"
   public_key = "${file("staging_key.pub")}"
 }
+
+# The aws_access_key_id and aws_secret_access_key must be obtained from
+# ~/.aws/credentials in the local environment.
