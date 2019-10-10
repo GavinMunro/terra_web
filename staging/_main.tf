9023397841676e86edd764eb09c1c91544b8605b
gavin <ob1.data@gmail.com>
2019-10-10 22:47:10 +1000

    Try specifying full path to public key file

--- staging/_main.tf
+++ staging/_main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 resource "aws_key_pair" "key" {
   key_name   = "${var.key_name}"
-  public_key = "${file("staging_key.pub")}"
+  public_key = "${file("~/.aws/aws_terraform.pub")}"  #"aws_terraform.pub"
 }
 
 # The aws_access_key_id and aws_secret_access_key must be obtained from
-# ~/.aws/credentials in the local environment.
+# the AWS CLI location ~/.aws/credentials in the local environment.
